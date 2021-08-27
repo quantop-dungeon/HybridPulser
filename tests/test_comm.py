ae02cfaff1be96152d52027c06616bc97c347865
SAFedorov <fedorov.s.a@outlook.com>
2021-08-27 23:33:48 +0200

    Replaced functions for communication with the FPGA target with a dedicated class

--- tests/test_comm.py
+++ tests/test_comm.py
@@ -1,4 +1,4 @@
-from riopulse.fpga_communication import program
+from riopulse.pulsegen import PulseGen
 from riopulse.sequence import Sequence
 
 seq = Sequence(nchannels=3, start_time=0)
@@ -7,7 +7,8 @@ seq.append_pulse(1, 1.2e-3, 0.3e-3)
 seq.append_pulse(1, 0.3e-3, 0.6e-3)
 seq.stop_time = 10e-3  # (s)
 
-program(seq, resource='rio://172.22.11.2/RIO0')
+p = PulseGen('rio://172.22.11.2/RIO0')
+p.program(seq)
 
 # seq = Sequence(nchannels=3, start_time=0)
 # seq.append_pulse(0, 0.9e-6, 0.1e-6)
@@ -15,4 +16,5 @@ program(seq, resource='rio://172.22.11.2/RIO0')
 # seq.append_pulse(1, 2.5e-6, 0.5e-6)
 # seq.stop_time = 15e-6  # (s)
 
-# program(seq, resource='rio://172.22.11.2/RIO0')
+# p = PulseGen('rio://172.22.11.2/RIO0')
+# p.program(seq)
